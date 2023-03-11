3e72f0037b1e9a103761803bab0e0d3fce55b499
hlbrand <hlbrand2995@gmail.com>
2023-03-11 07:01:33 +0700

    14-Create-Login-API

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,43 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
-import React from "react";
+import React, { useEffect } from "react";
+import { getError } from "@/utils/error";
+import { toast } from "react-toastify";
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/router";
+import { signIn, useSession } from "next-auth/react";
 
 export default function LoginScreen() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || "/");
+    }
+  }, [router, session, redirect]);
+
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
-  const submitHandler = ({ email, password }) => {
-    console.log(email, password);
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (error) {
+      toast.error(getError(error));
+    }
   };
 
   return (
